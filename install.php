44fb9cbf890eb6bdc3e1e8f3f17b93a5d5a0debd
Vedran Ljubović <vljubovic@smartnet.ba>
2016-11-02 08:01:52 +0100

    Some fixes to install procedure

--- install.php
+++ install.php
@@ -2,15 +2,22 @@
 
 require("lib/config.php");
 
-// Fix ownership of some files
-`chgrp $conf_c9_group $conf_base_path/log`;
-`chmod 775 $conf_base_path/log`;
 
-`chgrp $conf_c9_group $conf_base_path/watch`;
-`chmod 775 $conf_base_path/watch`;
+// Create c9 user
+`groupadd $conf_c9_group`;
+`useradd $conf_c9_user -g $conf_c9_group -m`;
+`mkdir /home/$conf_c9_user/workspace`;
+`chown $conf_c9_user /home/$conf_c9_user/workspace`;
 
-`chown www-data $conf_base_path/data`;
+// Create some directories and set permissions (git doesn't permit empty directories)
+$directories=array("log", "watch", "data", "c9fork", "web/buildservice");
+foreach($directories as $dir) {
+	`mkdir $conf_base_path/$dir`;
+	`chgrp $conf_c9_group $conf_base_path/$dir`;
+	`chmod 775 $conf_base_path/$dir`;
+}
 
+// FIXME?
 `touch $conf_base_path/log/admin.php.log`;
 `chown www-data log/admin.php.log`;
 `touch $conf_base_path/log/autotest.log`;
@@ -22,6 +29,7 @@ echo Downloading Cloud9 IDE
 echo Installing Cloud9 IDE
 `cd $conf_base_path/c9fork; ./scripts/install_sdk.sh`;
 `chmod 755 $conf_base_path/c9fork -R`;
+`cd /home/$conf_c9_user; ln -s $conf_base_path/c9fork fork`;
 
 // Do we need this?
 `chmod 644 $conf_base_path/c9fork/build/standalone/skin/default/*`;
@@ -38,15 +46,9 @@ echo Downloading Buildservice
 `cp $conf_base_path/web/buildservice.c9/* $conf_base_path/web/buildservice`;
 `rm -fr $conf_base_path/web/buildservice.c9`;
 
-// Prepare c9 home
-`groupadd $conf_c9_group`;
-`useradd $conf_c9_user -g $conf_c9_group -m`;
-`mkdir $conf_v1_workspace_path`;
-
 // Prepare SVN path
 `mkdir $conf_svn_path`;
 `chown $c9user:$c9group $conf_svn_path`;
-
 
 // Allow web scripts to use sudo to execute system-level webide commands
 `echo >> /etc/sudoers`;
