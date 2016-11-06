ee9f1975bd317be7cf48ec68cc49ab0ee36f4075
Vedran Ljubović <vljubovic@smartnet.ba>
2016-11-06 18:03:49 +0100

    reconstruct.php rewritten (code was for 1.0), now mostly working

--- lib/reconstruct.php
+++ lib/reconstruct.php
@@ -1,122 +1,134 @@
 <?php
 
-# Run as root
+// RECONSTRUCT.PHP - a handy script to recreate older versions of any file from
+// .stats file, if svn or git versions are not available for some reason
+
+// Run as root
 
 require(dirname(__FILE__) . "/../lib/config.php");
+require(dirname(__FILE__) . "/../lib/webidelib.php");
  
-$svn_ignore = array(".c9", ".svn", ".tmux", ".user", ".svn.fifo", ".inotify_pid", ".nakignore");
-$vrijeme_kreiranja = 10;
-$vrijeme_limit = 60;
-
-//$svn_base = $workspace_path . "/svn/workspace";
-$svn_base = $workspace_path . "/svn";
-$prefix = "";
-
-// Parametri
-if ($argc != 3) { 
-	die("GRESKA: reconstruct.php ocekuje tacno dva parametra\n");
+ 
+// Parameters
+if ($argc != 4 && $argc != 1) { 
+	print "ERROR: reconstruct.php expects exactly three parameters\n\n";
 }
+if ($argc != 4) {
+	print "reconstruct.php - a handy script to recreate older versions of any file from\nstats file, if svn or git versions are not available for some reason.\nRun userstats before using reconstruct!\n\n";
+
+	die("Usage:\n\tphp reconstruct.php USERNAME FILENAME TIMESTAMP\n\n");
+}
+
 $username = $argv[1];
 $filename = $argv[2];
+$timestamp = $argv[3];
 
 //$action = $argv[2];
 //$prefix = "/$username"; // Ukloniti kada svaki user bude imao svoj repo
 
-ucitaj_statistiku($username);
-reconstruct_file($username, $filename);
-print "Završen reconstruct\n";
+read_stats($username);
+reconstruct_file($username, $filename, $timestamp);
 
 exit(0);
 
 
 
-// Funkcije
+// Functions
 
-function ucitaj_statistiku($username) {
-	global $username, $base_path, $stats;
-	$stat_path = $base_path . "/stats/$username.stats";
+// Read stats file
+function read_stats($username) {
+	global $stats, $conf_stats_path;
+
+	$username_efn = escape_filename($username);
+	$stat_file = $conf_stats_path . "/" . "$username_efn.stats";
+	
 	$stats = NULL;
-	if (file_exists($stat_path))
-		eval(file_get_contents($stat_path));
+	if (file_exists($stat_file))
+		eval(file_get_contents($stat_file));
 	if ($stats == NULL) {
 		$stats = array(
 			"global_events" => array(),
 			"last_update_rev" => 0
 		);
 	}
-}
-
-// Sortiranje svn loga po vremenu commita u rastućem redoslijedu
-function svnsort($a, $b) {
-	if ($a['unixtime'] == $b['unixtime']) return 0;
-	return ($a['unixtime'] < $b['unixtime']) ? -1 : 1;
-}
-
-
-function evtsort($a, $b) {
-	if ($a['time'] == $b['time']) return 0;
-	return ($a['time'] < $b['time']) ? -1 : 1;
-}
-
-function reconstruct_file($username, $filename) {
-	global $stats, $base_path;
-	
-	if (!array_key_exists($filename, $stats))
-		die("GRESKA: Nemam taj fajl u logu");
-		
-	$fille = array();
-	$lastline = 0;
-	foreach($stats[$filename]['events'] as $event) {
-		if ($event['text'] == "created") {
-			if (!array_key_exists('content',$event) || empty($event['content']) || $event['content'] == "binary")
-				continue;
-			$linije = explode("\n", $event['content']);
-			for ($i=0; $i<count($linije); $i++) {
-				$fille[$i+1] = $linije[$i];
-				if ($i+1 > $lastline) $lastline = $i+1;
-				print "CREATE ".($i+1)." ".$linije[$i]."\n";
-			}
-		}
-		if ($event['text'] == "modified") {
-			foreach($event['diff'] as $difftype => $diffentry) {
-				if ($difftype == "change") {
-					foreach($diffentry as $lineno => $content) {
-						$fille[$lineno] = $content;
-						if ($lineno > $lastline) $lastline = $lineno;
-						print "CHANGE $lineno $content\n";
-					}
-				}
-				if ($difftype == "add_lines") {
-					$prva = true;
-					foreach($diffentry as $lineno => $content) {
-						if ($prva && strlen(trim($content)) > 2) {
-							$fille[$lineno] = $content;
-							print "ADD->CHANGE $lineno $content\n";
-							$prva = false;
-							continue;
-						}
-						$prva = false;
-						for ($i=$lastline; $i>=$lineno; $i--)
-							$fille[$i+1] = $fille[$i];
-						$lastline++;
-						$fille[$lineno] = $content;
-						print "ADD $lineno $content\n";
-					}
-				}
-			}
+	// Stats file can reference other files to be included
+	foreach ($stats as $key => $value) {
+		if (is_array($value) && array_key_exists("goto", $value)) {
+			$goto_path = $conf_stats_path . "/" . $value['goto'];
+			eval(file_get_contents($goto_path));
+			foreach($stats_goto as $ks => $vs)
+				$stats[$ks] = $vs;
+			$stats_goto = null;
 		}
 	}
-	
-	if (empty($fille))
-		die("GRESKA: Nije pronadjen nikakav sadrzaj u fajlu $filename");
-	
-	file_put_contents("/tmp/gen.c", join("\n", $fille));
 }
 
 
-function debug_log($msg) {
-	$time = date("d. m. Y. H:i:s");
-	`echo $time $msg >> /tmp/rename.log`;
+function reconstruct_file($username, $filename, $timestamp) {
+	global $stats, $base_path;
+	
+	$userdata = setup_paths($username);
+	
+	if (!array_key_exists($filename, $stats))
+		die("ERROR: File doesn't exist in stats log");
+		
+	$file_workspace_path = $userdata['workspace'] . "/$filename";
+	if (!file_exists($file_workspace_path)) {
+		print "File doesn't exist in workspace...\nCreating from blank (possibly with errors!)\n\n";
+		$work_file = array();
+	} else
+		$work_file = file($file_workspace_path);
+	
+	$file_log = $stats[$filename]['events'];
+	$evtcount = count($file_log);
+	
+	// We reconstruct the file backwards from its current state
+	for ($i=$evtcount-1; $i>=0; $i--) {
+		//print "$i,";
+		if ($file_log[$i]['time'] < $timestamp) break;
+		if ($i < -$timestamp) break;
+		if ($file_log[$i]['text'] != "modified") continue;
+		
+		if (array_key_exists("change", $file_log[$i]['diff']))
+			foreach($file_log[$i]['diff']['change'] as $lineno => $text) {
+				// Since php arrays are associative, we must initialize missing members in correct order
+				if ($lineno-1 > count($work_file))
+					for ($j=count($work_file); $j<$lineno; $j++)
+						$work_file[$j] = "\n";
+				$work_file[$lineno-1] = $text . "\n";
+			}
+		if (array_key_exists("remove_lines", $file_log[$i]['diff'])) {
+			$offset=-1;
+			foreach($file_log[$i]['diff']['remove_lines'] as $lineno => $text) {
+				if ($lineno+$offset > count($work_file))
+					for ($j=count($work_file); $j<$lineno+$offset+1; $j++)
+						$work_file[$j] = "\n";
+				if ($text == "false" || $text === false) $text = "";
+				array_splice($work_file, $lineno+$offset, 0, $text . "\n");
+				//$offset++;
+			}
+		}
+		if (array_key_exists("add_lines", $file_log[$i]['diff'])) {
+			$offset=-1;
+			foreach($file_log[$i]['diff']['add_lines'] as $lineno => $text) {
+				if ($lineno-$offset > count($work_file))
+					for ($j=count($work_file); $j<$lineno-$offset+1; $j++)
+						$work_file[$j] = "\n";
+				array_splice($work_file, $lineno-$offset, 1);
+				$offset++;
+			}
+				if ($i == 870) { print "lineno $lineno offset $offset\n"; break; }
+		}
+//		if ($i == 878) { break; }
+	}
+	
+	$output_path = "/tmp/reconstruct";
+	if (!file_exists($output_path)) mkdir($output_path);
+	$output_path .= "/" . basename($filename);
+	
+	file_put_contents($output_path, join("", $work_file));
+	print "Written file $output_path, merged " . ($evtcount-$i) . " changes.\n";
 }
+
 
 ?>
