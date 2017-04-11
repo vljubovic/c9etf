0a517f702e86ef5116413307dad35681f6850e10
Vedran Ljubović <vljubovic@smartnet.ba>
2017-04-11 13:38:45 +0200

    More patches

--- install.php
+++ install.php
@@ -118,10 +118,20 @@ echo "Downloading Buildservice\n";
 
 // APPLY PATCHES
 
+echo "\nApplying patches:\n";
+
 // Use relative paths instead of absolute (so that e.g. http://hostname/user/ redirects to 
 // http://hostname/user/ide.html and not http://hostname/ide.html)
 echo "relative_paths.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/relative_paths.diff`;
+
+// Display a progress bar that corresponds to actual files being loaded
+echo "progress_bar.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/progress_bar.diff`;
+
+// Retry loading a failed file instead of just die
+echo "retry_failed.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/retry_failed.diff`;
 
 // Disable "keys" plugin which is confusing and uses screen real estate
 echo "disable_keys_plugin.diff\n";
@@ -135,22 +145,38 @@ echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/disable_welcome_plugin.d
 echo "disable_open_terminal.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/disable_open_terminal.diff`;
 
+// Disable "Process list" plugin that would enable users to kill eachothers processes
+echo "disable_processlist.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/disable_processlist.diff`;
+
+// Disable "Command" widget that allows user to run arbitrary command on server
+echo "disable_command.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/disable_command.diff`;
+
+// Disable "Show Home in Favorites" option (in tree) since students would 
+// accidentally enable it and then delete important files 
+echo "disable_show_home_in_favorites.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/disable_show_home_in_favorites.diff`;
+
 // Additional C/C++ snippets by cyclone
 echo "c_cpp_snippets.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/c_cpp_snippets.diff`;
 
-// Display a progress bar that corresponds to actual files being loaded
-echo "progress_bar.diff\n";
-echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/progress_bar.diff`;
+// Source code formatting for C, C++ and Java using Astyle
+// FIXME: Currently implemented through a web service - move to c9 api
+echo "formatter_astyle.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/formatter_astyle.diff`;
 
-// Retry loading a failed file instead of just die
-echo "retry_failed.diff\n";
-echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/retry_failed.diff`;
-
-// Fix tmux errors caused by many users/processes sharing the same socket for 
-// their tmux connections at the same time
+// By default tmux binary creates a socket in /tmp (owned by user)... this
+// sometimes caused permission errors when another user tried to run program.
+// This patch creates a separate tmux socket file for each user
 echo "tmux_socket_error.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/tmux_socket_error.diff`;
+
+// Use tmux binary that came with the OS and not the one compiled by Cloud9
+// (improves memory sharing)
+echo "use_system_tmux.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/use_system_tmux.diff`;
 
 // Detect when user is logged out and redirect to login page, instead of just
 // displaying "Reconnecting" forever (maybe localize?)
@@ -161,19 +187,74 @@ echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/detect_logout.diff`;
 echo "fix_forget_settings.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/fix_forget_settings.diff`;
 
-// Avoid pointless ENOENT error message for missing watch file
+// "Run" button would sometimes get stuck in disabled state
+echo "fix_stuck_run_button.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/fix_stuck_run_button.diff`;
+
+// Avoid inconsequential ENOENT error message that watch file had dissapeared
 echo "fix_useless_error.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/fix_useless_error.diff`;
+
+// Fix erroneous error message "No error" that showed up after using gdb
+echo "fix_gdb_no_error.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/fix_gdb_no_error.diff`;
+
+// Due to poor handling of undefined value, "debug" button would sometimes
+// automatically reenable instead of staying disabled
+echo "debugging_default_to_false.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/debugging_default_to_false.diff`;
+
+// Support for evaluating vectors and their elements in gdb debugger
+// (see http://stackoverflow.com/questions/253099/how-do-i-print-the-elements-of-a-c-vector-in-gdb)
+echo "gdb_evaluate_vectors_elements.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_evaluate_vectors_elements.diff`;
+
+// Debugging the debugger is painful due to async file writes - this fixes it at the cost of 
+// performance (we're not always debugging the debugger)
+// Also log file is stored in users workspace and timestamp added
+echo "gdb_sync_log.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_sync_log.diff`;
+
+// Don't crash debugger when there is open brace (but not closed!) inside string
+// Also some warnings
+echo "gdb_fix_brace_inside_string.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_fix_brace_inside_string.diff`;
+
+// Make sure truncated packages (sometimes returned by gdb) are valid JSON
+// Such packages sometimes caused crashes
+echo "gdb_fix_truncated_package.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_fix_truncated_package.diff`;
+
+// Properly escape octal numbers in gdb output (also sometimes causing crashes)
+echo "gdb_fix_escaping_octal_numbers.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_fix_escaping_octal_numbers.diff`;
+
+// Cleanup some warnings thrown by nodejs in gdb shim code, add some more debugging
+echo "gdb_warnings_debugging.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/gdb_warnings_debugging.diff`;
 
 // Use our URL for logout menu action
 echo "logout_url.diff\n";
 echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/logout_url.diff`;
 
-// Use tmux binary that came with the OS and not the one compiled by Cloud9
-// improves memory sharing
-echo "use_system_tmux.diff\n";
-echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/use_system_tmux.diff`;
+// Add "Send Homework" button (etf.zamger plugin) to Toolbar - don't enable 
+// this patch if you disable etf.zamger plugin, otherwise c9 may not run!
+echo "add_send_homework_to_toolbar.diff\n";
+echo `cd $conf_base_path/c9fork; patch -p1 < ../patches/add_send_homework_to_toolbar.diff`;
 
+// Patches that failed to port:
+// - Remove "Enable Auto-Save" from preferences but leave enabled (was in plugins/c9.ide.save/autosave.js)
+// - parse_program_output.diff is currently considered obsolete, but it's the only way to detect if program run too long
+// - gdb prevent infinite recursion, e.g. with evaluating C++ string object on stack 
+// - gdb evaluate C++ vector object on stack
+// - gdb limit number of children fetched to prevent slowness/crashes with oversized response package
+
+
+// Disable some runners: all C and C++ runners (we provide our own) and Shell command runners
+`mkdir $conf_base_path/c9fork/runners.disabled`;
+`mv $conf_base_path/c9fork/plugins/c9.ide.run/runners/C\ * $conf_base_path/c9fork/runners.disabled`;
+`mv $conf_base_path/c9fork/plugins/c9.ide.run/runners/C\+\+* $conf_base_path/c9fork/runners.disabled`;
+`mv $conf_base_path/c9fork/plugins/c9.ide.run/runners/Shell* $conf_base_path/c9fork/runners.disabled`;
 
 
 // Done
