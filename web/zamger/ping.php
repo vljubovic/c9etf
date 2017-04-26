eec23c76e62e28824625c5cb0f6868373878d2ef
vljubovic <vljubovic@smartnet.ba>
2017-04-26 12:10:44 +0200

    Update last file directly, for performance reasons

--- web/zamger/ping.php
+++ web/zamger/ping.php
@@ -4,28 +4,32 @@
 
 require_once(__DIR__."/config.php");
 require_once(__DIR__."/jsonlib.php");
-$webide_path = "/usr/local/webide";
+$conf_base_path = "/usr/local/webide";
+require_once($conf_base_path . "/lib/webidelib.php");
 
 session_start();
 if (!isset($_SESSION['server_session'])) {
-	print "ERROR";
+	print "ERROR ".$_SESSION['login'];
 } else {
 	$parameters = array();
 	$parameters[session_name()] = $_SESSION['server_session'];
 	$username_esa = escapeshellarg($_SESSION['login']);
+	$username_efn = escape_filename($_SESSION['login']);
 	
 	//$result = json_request_retry($conf_json_base_url . "ping.php", $parameters);
 	
 
 	//if ($result['success'] == "true")
-		if (file_exists($webide_path . "/broadcast.txt"))
-			print "BROADCAST: " .file_get_contents($webide_path . "/broadcast.txt");
+		if (file_exists($conf_base_path . "/broadcast.txt"))
+			print "BROADCAST: " .file_get_contents($conf_base_path . "/broadcast.txt");
 		else //print join("\n", $result['data']);
-			print "OK";
+			print "OK " . $_SESSION['login'];
 	/*else
 		print "ERROR";*/
 	
-	proc_close(proc_open("sudo $webide_path/bin/webidectl last-update $username_esa &", array(), $foo));
+	//proc_close(proc_open("sudo $conf_base_path/bin/webidectl last-update $username_esa &", array(), $foo));
+	$lastfile = $conf_base_path . "/last/$username_efn.last";
+	file_put_contents($lastfile, time());
 }
 
 ?>
