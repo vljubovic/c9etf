e827e34a6fa09880e3fdb47b1ca7ef3cf73e7aca
Vedran Ljubović <vljubovic@smartnet.ba>
2017-10-05 20:55:40 +0200

    Don't hang during install

--- lib/ensure_running.php
+++ lib/ensure_running.php
@@ -8,12 +8,12 @@ require(dirname(__FILE__) . "/webidelib.php");
 // check_php script that stops/starts php
 $check_php=`ps aux | grep check_php | grep -v grep`;
 if (!$check_php)
-	proc_close(proc_open("nohup $conf_base_path/lib/check_php &", array(), $foo));
+	proc_close(proc_open("nohup $conf_base_path/lib/check_php > /dev/null 2>&1 &", array(), $foo));
 
 // Generate stats
 $stats_monitor=`ps aux | grep stats_monitor.php | grep -v grep`;
 if (!$stats_monitor)
-	proc_close(proc_open("nohup php $conf_base_path/lib/stats_monitor.php > $conf_base_path/server_stats.log &", array(), $foo));
+	proc_close(proc_open("nohup php $conf_base_path/lib/stats_monitor.php > $conf_base_path/server_stats.log 2>&1 &", array(), $foo));
 
 // Check for ssh tail processes that copy stats from other nodes to control node
 $is_control_node = false;
@@ -30,7 +30,7 @@ if ($is_control_node) {
 			$ssh_tail_proc = `ps aux | grep $proc | grep -v grep`;
 			$log_file_name = $node['name'] . "_stats.log";
 			if (!$ssh_tail_proc)
-				proc_close(proc_open("nohup $ssh_tail -f $conf_base_path/server_stats.log > $conf_base_path/$log_file_name &", array(), $foo));
+				proc_close(proc_open("nohup $ssh_tail -f $conf_base_path/server_stats.log > $conf_base_path/$log_file_name 2>&1 &", array(), $foo));
 		}
 	}
 }
