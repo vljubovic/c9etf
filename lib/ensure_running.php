a9cd9779caf7075726832c4742fd8fb6d6e15f40
vljubovic <vljubovic@smartnet.ba>
2018-08-20 12:38:38 +0200

    Fixed ensure running

--- lib/ensure_running.php
+++ lib/ensure_running.php
@@ -1,6 +1,12 @@
 <?php
 
-// Ensure that all neccessary components are running
+// =========================================
+// ENSURE_RUNNING.PHP
+// C9@ETF project (c) 2015-2018
+//
+// Ensure that all neccessary components for webide are started
+// =========================================
+
 
 require(dirname(__FILE__) . "/config.php");
 require(dirname(__FILE__) . "/webidelib.php");
@@ -10,7 +16,7 @@ $check_php=`ps aux | grep check_php | grep -v grep`;
 if (!$check_php)
 	proc_close(proc_open("nohup $conf_base_path/lib/check_php > /dev/null 2>&1 &", array(), $foo));
 
-// Generate stats
+// Server stats monitoring (needed to see if there are sufficient resources)
 $stats_monitor=`ps aux | grep stats_monitor.php | grep -v grep`;
 if (!$stats_monitor)
 	proc_close(proc_open("nohup php $conf_base_path/lib/stats_monitor.php > $conf_base_path/server_stats.log 2>&1 &", array(), $foo));
@@ -27,7 +33,7 @@ if ($is_control_node) {
 	foreach ($conf_nodes as $node) {
 		if (!is_local($node['address'])) {
 			$ssh_tail = "ssh ".$node['address']." tail";
-			$ssh_tail_proc = `ps aux | grep $proc | grep -v grep`;
+			$ssh_tail_proc = `ps aux | grep "$ssh_tail" | grep -v grep`;
 			$log_file_name = $node['name'] . "_stats.log";
 			if (!$ssh_tail_proc)
 				proc_close(proc_open("nohup $ssh_tail -f $conf_base_path/server_stats.log > $conf_base_path/$log_file_name 2>&1 &", array(), $foo));
