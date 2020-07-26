7053dbd9e88ea2dffa32939c9ea8a3a885fad379
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-07-26 17:07:02 +0200

    Added admin login to /web/services/auth.php

--- web/services/auth.php
+++ web/services/auth.php
@@ -11,15 +11,23 @@ require_once("../login.php");
 if (isset($_POST['login'])) {
 	$login = $_POST['login'];
 	$pass = $_POST['password'];
+	$admin_login = $_POST['admin_login'];
 } else {
 	$input = json_decode(file_get_contents('php://input'),true);
 	if ($input) {
 		$login = $input['login'];
 		$pass = $input['password'];
+		$admin_login = $input['admin_login'];
 	}
 }
 
 $error = login($login, $_POST['password']);
+
+if($admin_login) {
+	if (!in_array($login,$conf_admin_users)) {
+		$error = "You are not an administrator!";
+	}
+}
 
 ini_set('default_charset', 'UTF-8');
 header('Content-Type: application/json; charset=UTF-8');
@@ -37,4 +45,4 @@ if ($error == "") {
 
 print json_encode($result);
 
-?>
+
