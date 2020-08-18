af988a6a953b4f0cf8e5c5f7119a0947425afc75
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-08-18 12:26:26 +0200

    Pre merge

--- web/zamger/config.default.php
+++ web/zamger/config.default.php
@@ -3,8 +3,7 @@
 // Konfiguracija Zamger web servisa
 
 global $conf_json_base_url, $conf_json_base_apiv5, $conf_json_max_retries;
-$conf_json_base_url = "http://zamger_web_1/";
-$conf_json_base_apiv5 = "http://zamger_api-v5_1/api-v5/";
+$conf_json_base_url = "https://zamger.etf.unsa.ba/";
+$conf_json_base_apiv5 = "https://zamger.etf.unsa.ba/api_v5/";
 $conf_json_max_retries = 3;
 
-?>
