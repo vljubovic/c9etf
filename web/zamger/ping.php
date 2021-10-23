b16a9aa0a1c3c6f216b748d8b1a399f8ca556c20
Vedran Ljubović <vljubovic@smartnet.ba>
2021-10-23 21:05:02 +0200

    Fixes to ping (now works as expected)

--- web/zamger/ping.php
+++ web/zamger/ping.php
@@ -9,23 +9,26 @@ require_once($conf_base_path . "/lib/webidelib.php");
 
 session_start();
 if (!isset($_SESSION['server_session'])) {
-	print "ERROR test";
+	print "OK session_expired";
 } else {
 	$parameters = array();
 	$parameters[session_name()] = $_SESSION['server_session'];
-	$username_esa = escapeshellarg($_SESSION['login']);
-	$username_efn = escape_filename($_SESSION['login']);
+	$login = $_SESSION['login'];
+	$username_esa = escapeshellarg($login);
+	$username_efn = escape_filename($login);
 		
 	//$result = json_request_retry($conf_json_base_url . "ping.php", $parameters);
 	
+	$users_file = $conf_base_path . "/users";
+	eval(file_get_contents($users_file));
 
-	//if ($result['success'] == "true")
+	if ($users[$login]['status'] == "active")
 		if (file_exists($conf_base_path . "/broadcast.txt"))
 			print "BROADCAST: " .file_get_contents($conf_base_path . "/broadcast.txt");
 		else //print join("\n", $result['data']);
 			print "OK " . $_SESSION['login'];
-	/*else
-		print "ERROR";*/
+	else
+		print "ERROR";
 	
 	//proc_close(proc_open("sudo $conf_base_path/bin/webidectl last-update $username_esa &", array(), $foo));
 	$lastfile = $conf_home_path . "/last/$username_efn.last";
